cb1d4a0760658cbe767fa10d45556a7cb0cf3c41
Paul McGrane <service@lynxtdc.com>
2015-04-24 17:05:42 -0400

    changing background on intro special

--- resources/views/pages/sections/price_matrix.blade.php
+++ resources/views/pages/sections/price_matrix.blade.php
@@ -96,7 +96,7 @@
 
 
 
-<div class="block-2 parallax parallax-bg-3">
+<div class="block-2 parallax parallax-bg-4">
     <div class="container">
         <div class="row">
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
